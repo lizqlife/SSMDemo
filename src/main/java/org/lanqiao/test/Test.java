776cbf153ad854ee3c533105ddd4574b400ecac2
lizq <lizhiqianglife@foxmail.com>
2017-09-09 16:32:13 +0800

    update Test.java

--- src/main/java/org/lanqiao/test/Test.java
+++ src/main/java/org/lanqiao/test/Test.java
@@ -9,8 +9,7 @@ import java.io.FileNotFoundException;
  */
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
-        String s;
-
+        System.out.println("Hello World");
     }
 
 }
